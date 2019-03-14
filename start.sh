3927e522c434948473cb4a6c2c4903ad0b780da7
parkeunsoo <1426312@naver.com>
2019-03-14 10:37:17 +0900

    update 0314

---
+++ start.sh
@@ -0,0 +1,12 @@
+# 키저장소 삭제
+rm -rf hfc-key-store
+rm -rf hfc-key-store2
+rm -rf hfc-key-store3
+rm -rf hfc-key-store4
+# CA 0,1,2,3 관리자 등록
+nodejs enrollAdmin.js
+nodejs enrollAdmin2.js
+nodejs enrollAdmin3.js
+nodejs enrollAdmin4.js
+# 서버 가동 
+nodejs server.js
